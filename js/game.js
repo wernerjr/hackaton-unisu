5d62d08f3d82b86de3ef30ab9a480eae1f1ac95e
Werner gehrke Junior <werner.junior@hbsis.com.br>
2021-05-11 22:45:37 -0300

    Movimentação e background

--- js/game.js
+++ js/game.js
@@ -1,39 +1,47 @@
-
-/* Game namespace */
+/* game namespace */
 var game = {
-
-    // an object where to store game information
+    /**
+     * an object where to store game global data
+     */
     data : {
-        // score
-        score : 0
+      score : 0
     },
-
-
+  
     // Run on page load.
-    "onload" : function () {
-        // Initialize the video.
-        if (!me.video.init(960, 640, {parent : "screen", scale : "auto", scaleMethod : "flex"})) {
-            alert("Your browser does not support HTML5 canvas.");
-            return;
-        }
-
-        // Initialize the audio.
-        me.audio.init("mp3,ogg");
-
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
-        me.loader.preload(game.resources, this.loaded.bind(this));
+    onload : function () {
+      // Initialize the video.
+      if (!me.video.init(1920, 1088, {parent : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+      }
+  
+      // Initialize the audio.
+      me.audio.init("mp3,ogg");
+  
+      // set all ressources to be loaded
+      me.loader.preload(game.resources, this.loaded.bind(this));
     },
-
+  
     // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
-
-        // Start the game.
-        me.state.change(me.state.PLAY);
-    }
-};
+/**
+ * callback when everything is loaded
+ */
+ loaded : function () {
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new game.PlayScreen());
+  
+    // register our player entity in the object pool
+    me.pool.register("mainPlayer", game.PlayerEntity);
+  
+    // enable the keyboard
+    me.input.bindKey(me.input.KEY.LEFT,  "left");
+    me.input.bindKey(me.input.KEY.RIGHT, "right");
+    // map X, Up Arrow and Space for jump
+    me.input.bindKey(me.input.KEY.X,      "jump", true);
+    me.input.bindKey(me.input.KEY.UP,     "jump", true);
+    me.input.bindKey(me.input.KEY.SPACE,  "jump", true);
+  
+    // start the game
+    me.state.change(me.state.PLAY);
+  }
+  };
